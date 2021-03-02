94f1b2b1c84cd9b14de8ff9ed0aa8eed87de4583
das <don.a.sarge@gmail.com>
2021-03-02 09:35:13 -0500

    3/2/21

--- app_das.py
+++ app_das.py
@@ -1,3 +1,4 @@
+
 # import dependencies
 import numpy as np
 import datetime as dt
@@ -24,26 +25,24 @@ Station = Base.classes.station
 # ------------------------------------------------------
 session = Session(engine)
 
-# Design a query to find the most active stations (what stations have the most rows?)
-# List the stations and the counts in descending order.
 
-station_max = session.query(Measurement.station, func.count(Measurement.station)).group_by(
-    Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+# station_max = session.query(Measurement.station, func.count(Measurement.station)).group_by(
+#     Measurement.station).order_by(func.count(Measurement.station).desc()).all()
 
-# create DataFrame using data
-df = pd.DataFrame(station_max, columns=['station', 'count'])
+# # create DataFrame using data
+# df = pd.DataFrame(station_max, columns=['station', 'count'])
 
-max_df = df.loc[df['count'].idxmax()]
+# max_df = df.loc[df['count'].idxmax()]
 
 
-# find the last date in the database
-last_date = session.query(Measurement.date).order_by(
-    Measurement.date.desc()).first()
+# # find the last date in the database
+# last_date = session.query(Measurement.date).order_by(
+#     Measurement.date.desc()).first()
 
-# Calculate the date 1 year ago from the last data point in the database
-query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
-
-session.close()
+# # Calculate the date 1 year ago from the last data point in the database
+# #calc_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+# calc_date = dt.date(last_date) - dt.timedelta(days=365)
+# session.close()
 # ---------------------------------------------------------
 # Create an app
 app = Flask(__name__)
@@ -60,28 +59,30 @@ def home():
         f"Welcome to Hawaii Climate Page<br/> "
         f"Available Routes:<br/>"
         f"<br/>"
-        f"The list of precipitation data with dates:<br/>"
+        f"1. - The list of precipitation data with dates:<br/>"
         f"Precipitation <a href='/api/v1.0/precipitation' target='_blank'>/api/v1.0/precipitation</a><br/>"
-        f"<br/>"
-        f"The list of stations and names:<br/>"
+        f"------------------------------------------<br/>"
+        f"2. - The list of stations and names:<br/>"
         f"Active_Stations <a href='/api/v1.0/stations' target='_blank'>/api/v1.0/stations</a><br/>"
+        f"--------------------------------------------<br/>"
+        f"3. - The most active station:{(max_df)}<br/>"
         f"<br/>"
-        f"The most active station:{(max_df)}<br/>"
+        f"First date entered:{first_date}<br/>"
         f"<br/>"
-        f"The list of temperture observations for the most active weather station from a year from the last data point:<br/>"
-        f"/api/v1.0/tobs<br/>"
+        f"Date 1 year before last date entered:{calc_date}<br/>"
         f"<br/>"
-        f"Min, Max. and Avg. temperatures for given start date: (please use 'yyyy-mm-dd' format):<br/>"
-        f"/api/v1.0/min_max_avg/&lt;start date&gt;<br/>"
+        f"Last date entered:{last_date}<br/>"
+        f"--------------------------------------------<br/>"
+        f"4. - The list of temperture observations for the most active weather station from a year from the last data point:<br/>"
         f"<br/>"
-        f"Min. Max. and Avg. tempratures for given start and end date: (please use 'yyyy-mm-dd'/'yyyy-mm-dd' format for start and end values):<br/>"
-        f"/api/v1.0/min_max_avg/&lt;start date&gt;/&lt;end date&gt;<br/>"
+        f"<a href='/api/v1.0/min_max_avg/2016-08-23/2017-08-23' target='_blank'>/api/v1.0/tobs</a><br/>"
+        f"----------------------------------------------<br/>"
+        f"5. - Min. Max. and Avg. tempratures for given start and end date:<br/>"
         f"<br/>"
-        f"<a href='/api/v1.0/min_max_avg/2012-01-01/2016-12-31' target='_blank'>/api/v1.0/min_max_avg/2012-01-01/2016-12-31</a>"
+        f"temperature by date <a href='/api/v1.0/min_max_avg/2012-01-01/2016-12-31' target='_blank'>/api/v1.0/min_max_avg</a>"
     )
 # ------------------------------------------------------
-
-# create precipitation route
+# 1. - create precipitation route
 
 
 @app.route("/api/v1.0/precipitation")
@@ -105,8 +106,7 @@ def precipitation():
     return jsonify(precipitation)
 
 # -----------------------------------------------
-
-# create stations route
+# 2. - create stations route
 
 
 @app.route("/api/v1.0/stations")
@@ -130,21 +130,40 @@ def stations():
     # jsonify the list
     return jsonify(station_list)
 # -----------------------------------------------
+# 3. - Design a query to find the most active stations
+# List the stations and the counts in descending order.
+# Find most active atation
 
-# # create most_active_station route
+
+station_max = session.query(Measurement.station, func.count(Measurement.station)).group_by(
+    Measurement.station).order_by(func.count(Measurement.station).desc()).all()
+
+# create DataFrame using data
+df = pd.DataFrame(station_max, columns=['station', 'count'])
+
+max_df = df.loc[df['count'].idxmax()]
+
+# -------------------------------------------------------
+# find the first date in the database
+first_date = session.query(Measurement.date).order_by(
+    Measurement.date.asc()).first()
+
+# ----------------------------------------------------
+
+# find the last date in the database
+last_date = session.query(Measurement.date).order_by(
+    Measurement.date.desc()).first()
+
+# --------------------------------------------------------------
 
 
-# @app.route("/api/v1.0/MaxStations")
-# def MaxStations():
-#     # Create the session link
-#     session = Session(engine)
-#     return f"most active station"
+# Calculate the date 1 year ago from the last data point in the database
+calc_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+session.close()
 
 
 # -------------------------------------------------------
-
-# create temperatures route
-
+# 4. - create temperatures route
 
 @app.route("/api/v1.0/tobs")
 def tobs():
@@ -153,60 +172,36 @@ def tobs():
 
     """Return a JSON list of Temperature Observations (tobs) for the previous year."""
     # query tempratures from a year from the last data point.
-    # query_date  is "2016-08-23" for the last year query
-    results = session.query(Measurement.tobs, Measurement.date).filter(
-        Measurement.date >= query_date).all()
+    # calc_date  is "2016-08-23" for the last year query
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= '2016-08-23').all()
 
     session.close()
 
-    # convert list of tuples to show date and temprature values
-    tobs_list = []
-    for result in results:
-        r = {}
-        r["date"] = result[1]
-        r["temprature"] = result[0]
-        tobs_list.append(r)
-
-    # jsonify the list
-    return jsonify(tobs_list)
-
-# -------------------------------------------------------
-
-# create route for query the most active station for the last year of data
-
-
-@app.route("/api/v1.0/min_max_avg/<start>")
-def start(start):
-    # create session link
-    session = Session(engine)
-
-    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date."""
-
-    # take any date and convert to yyyy-mm-dd format for the query
-    start_dt = dt.datetime.strptime(start, '%Y-%m-%d')
-
-    # query data for the start date value
-    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(
-        Measurement.tobs)).filter(Measurement.date >= start_dt).all()
-
-    session.close()
-
-    # Create a list to hold results
     t_list = []
     for result in results:
         r = {}
+        r["StartDate"] = start_dt
         r["StartDate"] = start_dt
         r["TMIN"] = result[0]
         r["TAVG"] = result[1]
         r["TMAX"] = result[2]
         t_list.append(r)
 
-    # jsonify the result
-    return jsonify(t_list)
+    # convert list of tuples to show date and temperature values
+    tobs_list = []
+    for result in results:
+        r = {}
+        r["date"] = result[1]
+        r["temperature"] = result[0]
+        tobs_list.append(r)
 
-# ------------------------------------------------------
+    # jsonify the list
+    return jsonify(tobs_list)
 
-# create route for theset start date to last date of data
+# -------------------------------------------------------
+# 5. - create route for query the most active station for the last year of data
 
 
 @app.route("/api/v1.0/min_max_avg/<start>/<end>")
@@ -244,4 +239,4 @@ def start_end(start, end):
 # ---------------------------------------------------------
 # run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="127.0.0.1", port=8010)
